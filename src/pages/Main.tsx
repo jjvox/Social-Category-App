9535d3c80d74c69ca2ca82b26ea0b3db48a21c83
재이 <>
2023-01-27 01:35:31 +0900

    text 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,23 +1,23 @@
-import GoogleLogin from "../components/LoginButton";
+import GoogleLogin from '../components/LoginButton';
 
 const MainPage = () => {
   return (
     <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
+      <div className='hero min-h-screen bg-base-200'>
+        <div className='hero-content flex-col lg:flex-row'>
           <img
-            src="https://placeimg.com/260/400/arch"
-            className="max-w-sm rounded-lg shadow-2xl"
+            src='https://placeimg.com/260/400/arch'
+            className='max-w-sm rounded-lg shadow-2xl'
           />
-          <div className="ml-4">
-            <h1 className="text-5xl font-bold">Social Cagegory Service</h1>
-            <p className="py-6">
+          <div className='ml-4'>
+            <h1 className='text-5xl font-bold'>Social Cagegory Service</h1>
+            <p className='py-6'>
               <strong>
-                1. 구독하고 계신 유튜브 크리에이터의 새로운 영상 !{" "}
+                1. 구독하고 계신 유튜브 크리에이터의 새로운 영상 !{' '}
               </strong>
               <br />
               <strong>
-                2. 팔로우 하고 있는 인스타 인플루언서의 새로운 피드!
+                2. 팔로우 하고 있는 인스타 인플루언서의 새로운 피드!!
               </strong>
               <br />
               <strong>3. 이웃 추가를 한 파워 블로그의 새로운 글! </strong>
